f3fdb71fc5e04150537d500fd8077e1e5282fcab
Shtrecker <gogels235@gmail.com>
2024-10-29 16:37:47 +0300

    Added logic for NV and torch from gunslinger

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -1799,6 +1799,14 @@ void CActor::OnItemDrop(CInventoryItem *inventory_item, bool just_before_destroy
 	if(outfit && inventory_item->m_ItemCurrPlace.type==eItemPlaceSlot)
 	{
 		outfit->ApplySkinModel	(this, false, false);
+
+		bool isGuns = EngineExternal()[EEngineExternalGunslinger::EnableGunslingerMode];
+		if (isGuns)
+		{
+			CTorch* torch = smart_cast<CTorch*>(inventory().ItemFromSlot(TORCH_SLOT));
+			if (torch && torch->IsSwitched())
+				torch->Switch(false);
+		}
 	}
 
 	CHelmet* helmet = smart_cast<CHelmet*>(inventory_item);
@@ -1807,6 +1815,13 @@ void CActor::OnItemDrop(CInventoryItem *inventory_item, bool just_before_destroy
 		CTorch* torch = smart_cast<CTorch*>(inventory().ItemFromSlot(TORCH_SLOT));
 		if (torch && torch->GetNightVisionStatus())
 			torch->SwitchNightVision(false);
+
+		bool isGuns = EngineExternal()[EEngineExternalGunslinger::EnableGunslingerMode];
+		if (isGuns)
+		{
+			if (torch && torch->IsSwitched())
+				torch->Switch(false);
+		}
 	}
 
 	CWeapon* weapon	= smart_cast<CWeapon*>(inventory_item);
